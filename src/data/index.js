cce677f3960b22dde06e5e81490059e40f12bd9d
ilyaseen19 <alfuty09@gmail.com>
2023-05-25 15:34:46 -0500

    publishing initial face of app

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,5 @@
 // const baseUrl = "http://192.168.1.13:9000/api/";
-const baseUrl = "http://192.168.43.54:9000/api/";
+// const baseUrl = "http://192.168.43.54:9000/api/";
+const baseUrl = "https://ghhamilton-api-service.onrender.com/api/";
 
 export { baseUrl };
