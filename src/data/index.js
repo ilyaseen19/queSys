fbe8b19bcd9eba442d2a0987b96c5aa28ca9f053
ilyaseen19 <alfuty09@gmail.com>
2023-05-25 15:38:43 -0500

    env update

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://192.168.1.13:9000/api/";
 // const baseUrl = "http://192.168.43.54:9000/api/";
-const baseUrl = "https://ghhamilton-api-service.onrender.com/api/";
+const baseUrl = process.env.BASE_URL || "";
 
 export { baseUrl };
