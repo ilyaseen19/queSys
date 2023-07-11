49a64f0991656e036d6ef9dbc71b516b4a769efe
ilyaseen19 <alfuty09@gmail.com>
2023-07-11 13:06:01 -0500

    data url

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://192.168.1.13:9000/api/";
-const baseUrl = "http://192.168.43.54:9000/api/";
-// const baseUrl = process.env.REACT_APP_BASE_URL;
+// const baseUrl = "http://192.168.43.54:9000/api/";
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export { baseUrl };
