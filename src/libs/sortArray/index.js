6f2b3059387aadffd8f463c6f4d52595b4d488f0
ilyaseen19 <alfuty09@gmail.com>
2023-07-13 09:10:56 -0500

    changed base url

--- src/libs/sortArray/index.js
+++ src/libs/sortArray/index.js
@@ -6,4 +6,12 @@ const sortArr = async (data) => {
   return ascSort;
 };
 
+const sortQue = async (data) => {
+  const ascSort = await data.sort(
+    (obj1, obj2) => new Date(obj1.createdAt) - new Date(obj2.createdAt)
+  );
+
+  return ascSort;
+};
+
 export { sortArr };
